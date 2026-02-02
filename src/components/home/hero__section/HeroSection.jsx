40bce29b30066dfd0c3e9e3076a6cd08f3ee4447
Premkmaurya <premkmaurya123456@gmail.com>
2026-02-02 10:45:01 +0530

    Enhance HeroSection with GSAP animations for improved visual appeal; update Home component for better structure

--- src/components/home/hero__section/HeroSection.jsx
+++ src/components/home/hero__section/HeroSection.jsx
@@ -2,9 +2,60 @@ import React from "react";
 import { RiArrowRightLine } from "@remixicon/react";
 import "./HeroSection.scss";
 
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import { SplitText } from "gsap/SplitText";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
 import CircularText from "../../react__bits__components/circular_text/CircularText";
 
 const HeroSection = () => {
+  gsap.registerPlugin(useGSAP);
+  gsap.registerPlugin(SplitText);
+  gsap.registerPlugin(ScrollTrigger)
+
+  let split = new SplitText(".hero__box-text", { type: "words,lines" });
+
+  useGSAP(() => {
+    const tl = gsap.timeline();
+    tl.from(".hero__title", {
+      y: 100,
+      opacity: 0,
+      duration: 1,
+      ease: "power4.out",
+    })
+      .from(
+        ".hero__box",
+        {
+          y: 100,
+          opacity: 0,
+          duration: 1,
+          ease: "power4.out",
+        },
+        "-=0.5",
+      )
+      .from(
+        split.lines,
+        {
+          duration: 0.5,
+          y: 100, // animate from 100px below
+          autoAlpha: 0, // fade in from opacity: 0 and visibility: hidden
+          stagger: 0.05,
+        },
+        "-=0.3",
+      )
+      .from(
+        ".hero__box-button",
+        {
+          y: 50,
+          opacity: 0,
+          duration: 0.8,
+          ease: "power4.out",
+        },
+        "-=0.3",
+      );
+  }, []);
+
   return (
     <section className="hero">
       <div className="hero__content">
--- src/pages/home_page/Home.jsx
+++ src/pages/home_page/Home.jsx
@@ -7,7 +7,11 @@ import AboutSection from '../../components/home/about__section/AboutSection'
 import ProductSection from '../../components/home/product__section/ProductSection'
 import FooterCard from '../../components/home/footer/FooterCard'
 
+
 const Home = () => {
+
+
+
   return (
     <div className='container'>
       <HeroSection />
